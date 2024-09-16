8e93a60830e8a51896e3dd5e6b7d4137725eb0ec
KKudas <143763586+KKudas@users.noreply.github.com>
2024-09-16 18:00:29 +0800

    Update order-service.js

--- order-service.js
+++ order-service.js
@@ -9,30 +9,53 @@ let orders = [];
 
 // POST /orders: Create a new order. This service will:
 app.post("/orders", async (req, res) => {
-  const data = req.body;
-  const prodService = axios.get("localhost:3001/health");
-  const customerService = axios.get("localhost:3002/health");
+  try {
+    const productId = parseInt(req.body.productId);
+    const customerId = parseInt(req.body.customerId);
 
-  const [res1, res2] = await Promise.all([prodService, customerService]);
+    const productReq = await axios.get(
+      `http://localhost:3001/products/${productId}`
+    );
+    const customerReq = await axios.get(
+      `http://localhost:3002/customers/${customerId}`
+    );
 
-  if (res1.data.status === "ok" && res2.data.status === "ok") {
+    const [customerRes, productRes] = await Promise.all([
+      customerReq,
+      productReq,
+    ]);
+
+    // Verify that the customer exists by communicating with the Customer Service.
+    // Verify that the product exists by communicating with the Product Service.
+    if (productRes.status !== 200 || customerRes.status !== 200) {
+      return res
+        .status(500)
+        .json({ error: "Failed to retrieve product or customer id" });
+    }
+
+    const data = req.body;
     orders.push(data);
-    res.status(201).json(data);
-  } else {
-    res.status(400).json({
-      message: "One or both APIs failed",
-      api1: response1.data,
-      api2: response2.data,
-    });
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
-// Verify that the customer exists by communicating with the Customer Service.
-// Verify that the product exists by communicating with the Product Service.
+
 // Create the order only if the customer and product are valid.
 
 // GET /orders/:orderId: Get order details.
-// app.get("/orders/:orderId", (req, res) => {});
+app.get("/orders/:orderId", (req, res) => {
+  const orderId = parseInt(req.params.orderId, 10);
+  const data = orders.find((order) => order.orderId === orderId);
+
+  if (data) {
+    res.json(data);
+  } else {
+    res.status(404).json({ message: "Order not found" });
+  }
+});
+
 // PUT /orders/:orderId: Update an order.
 
 // DELETE /orders/:orderId: Delete an order.
